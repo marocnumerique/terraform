e61d9298e3fce0f695291780c2170dc6a90a77fd
mcsalah <macsalah@gmail.com>
2020-06-15 22:46:24 +0100

    Update lbavaset.tf

--- lbavaset.tf
+++ lbavaset.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "test" {
 resource "azurerm_network_security_group" "nsgmulti" {
  
     name                = "mynsg"
-    location            = "eastus"
+    location            = "West US 2"
     resource_group_name = azurerm_resource_group.test.name
     
     security_rule {
