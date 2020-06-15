2ae1edf7216fa0af84d6a1a91bc57cc255e70307
mcsalah <macsalah@gmail.com>
2020-06-15 22:14:27 +0100

    Create config.tf

---
+++ config.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {
+  }
+  subscription_id = "c551d83d-898f-4b11-bf52-24f64a47bcac"
+  client_id       = "1d9813b6-e685-432e-8ce9-068c50e5210d"
+  client_secret   = "a65c7d79-2662-4bec-8d42-6c5a607ef4b6"
+  tenant_id       = "11ea4d91-4dc5-40fc-a8e5-6c073071b9fd"
+}
