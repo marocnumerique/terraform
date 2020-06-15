6f1b383528afa4c1e90f5363764666d0fff971cc
mcsalah <macsalah@gmail.com>
2020-06-15 22:40:52 +0100

    Update lbavaset.tf

--- lbavaset.tf
+++ lbavaset.tf
@@ -16,6 +16,43 @@ resource "azurerm_subnet" "test" {
  virtual_network_name = azurerm_virtual_network.test.name
  address_prefix       = "10.0.2.0/24"
 }
+resource "azurerm_network_security_group" "nsgmulti" {
+ 
+    name                = "mynsg"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.test.name
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+security_rule {
+        name                       = "web"
+        priority                   = 210
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    tags = {
+        environment = "nsg ssh + web"
+    }
+}
+resource "azurerm_network_interface_security_group_association" "assnsgmulti" {
+  
+    network_interface_id      = azurerm_network_interface.test.id
+    network_security_group_id = azurerm_network_security_group.nsgmulti.id
+}
 
 resource "azurerm_public_ip" "test" {
  name                         = "publicIPForLB"
